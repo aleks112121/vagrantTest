e8fcdeeff8b594217590008e3763755dbe062e08
Oleksii Mykytiuk <aleksmikipapa@gmail.com>
2018-07-05 10:37:10 +0300

    update v1.0

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 To test the program, you need to have up to date vagrant and VirtualBox (in the moment of writting: Vagrant 1.8.1+dfsg-1ubuntu0.2 and VirtualBox 5.1.34-dfsg-0ubuntu1.16.04.2)
 
 Then follow steps:
-1) clone the repository (if not yet)
-2) type repository directory type "vagrant up"
+    1) cd to repository folder (if not yet)
+    2) type "vagrant up"
 
 To make sure app works you can:
-  1) run php script on your host pc
-  2) type: nc 127.0.0.1 9999 <<< "nextid"
+    1) run php script on your host pc (you need to have php interpretator installed)
+    2) type: nc 127.0.0.1 9999 <<< "nextid"
--- server.php
+++ server.php
@@ -10,6 +10,7 @@ $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket
 echo "\nBinding Socket...";
 $result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
 
+//read and initialize number server will return
 $number = file_get_contents("database");
 
 echo "\nListening Socket...";
@@ -23,21 +24,24 @@ while(true)
         $input = socket_read($spawn, 1024) or die("Could not read input\n");
         
         $input = str_replace("\n", "", $input);
-
+        
+        //if input data valid reurn id for client
         if($input == "nextid")
         {
                 $output = "yourid: " . $number . "\n";
                 $number += 1;
 
                 $result = FALSE;
-
+                
+                //if file is blocked, try to write untill success
                 while($result == FALSE)
                 {
                         $result = file_put_contents("database", $number);
                 }
         }
 	else
-	{
+	{       
+                //if input data wrong inform client
                 $output = "Unknown command\n";
         }
 
